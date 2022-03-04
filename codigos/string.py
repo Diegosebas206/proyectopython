904cad3532dbd330dd000652d5b5809199f0f7d3
Diegosebas206 <diegodavalos2001@gmail.com>
2022-03-04 13:21:56 -0500

    tipos de datos

---
+++ codigos/string.py
@@ -0,0 +1,15 @@
+#ejemplo 1
+mensaje = "hola a todos"
+
+print(mensaje)
+
+#ejemplo 2
+mensaje1 = "hola"
+mensaje2 = "a todos"
+
+print(mensaje1, mensaje2)
+
+#ejemplo 2
+nombre = str(input("Escriba su nombre: "))
+
+print(nombre)
