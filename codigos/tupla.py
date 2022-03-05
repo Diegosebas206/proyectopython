19dfb47516a1da75b6936fd31a7738b744bc135a
Diegosebas206 <diegodavalos2001@gmail.com>
2022-03-04 21:56:00 -0500

    tipos de datos

--- codigos/tupla.py
+++ codigos/tupla.py
@@ -1,16 +1,17 @@
 #ejemplo 1
-tuple = [23,"Python","Diego",67]
-finaltuple = [x for x in zip(*[iter(tuple)])]
+tuple = 23,"Febrero","Abril",67
 
-print(finaltuple)
+print (tuple[1])
+print(type(tuple))
 
 #ejemplo 2
-tuple = [23,"Python","Diego",26]
-finaltuple = [x for x in zip(*[iter(tuple)]*2)]
+tuple = 23,"Python","Diego",26
 
-print(finaltuple)
+print(tuple)
+print(type(tuple))
 
 #ejemplo 3
-datos = [(4,3,2),('T','C','S')]
+datos = 4,3,2,'T','C','S'
 
 print("Lista: ",datos)
+print(type(datos))
